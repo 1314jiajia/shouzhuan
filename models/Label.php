7f6c16b68352649a84e263498e4fbbbeeaf49b20
Mr li <you@example.com>
2019-10-10 12:28:19 +0800

    应用修改有问题

--- models/Label.php
+++ models/Label.php
@@ -29,10 +29,7 @@ class Label extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            // [['label'],'required','message'=>'标签字段不能为空'],
-            // [['pid', 'created_at', 'updated_at'], 'integer'],
-            // [['pid','label'], 'string', 'max' => 50],
-
+           
             [['pid', 'created_at', 'updated_at'], 'integer'],
             [['label'], 'string', 'max' => 11],
         
